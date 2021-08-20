84f59d6e1a13cf8708c054d1fa7920dcf812d5eb
inaadhiakari <inaadhikari99@github.com>
2021-08-20 23:17:14 +0545

    project initiate

---
+++ mobile_pasal/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// route to go conroller and its function
+Route::get('admin',[AdminController::class,'index']);
+
+
+//route to accept the request post
+Route::post('admin/auth',[AdminController::class,'auth'])->name('admin.auth');
+
+
+Route::group(['middleware' =>'admin_auth'],function(){
+Route::get('admin/dashboard',[AdminController::class,'dashboard']);
+Route::get('admin/category',[categoryController::class,'index']);
+
+});
